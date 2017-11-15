e55654754233654f28010462732056206d211c5d
@slovacus <slovacus@gmail.com>
2017-11-15 08:08:25 -0500

    !Fix: order code

--- src/functions/example1.go
+++ src/functions/example1.go
@@ -1,20 +1,21 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 )
 
 func main() {
-  fmt.Println(suma(10, 10))
-  fmt.Println(sumaWithTwoResponse(10, 10))
+	fmt.Println(suma(10, 10))
+	fmt.Println(sumaWithTwoResponse(10, 10))
 }
 
 func suma(x int, y int) int {
-  return x + y
+	r := x + y
+	return r
 }
 
 func sumaWithTwoResponse(x int, y int) (response int, firstNumber int) {
-  response = x + y
-  firstNumber = x
-  return
+	response = x + y
+	firstNumber = x
+	return
 }
